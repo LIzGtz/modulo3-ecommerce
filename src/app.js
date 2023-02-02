f5706de71453e9458aa235aec5aee89c1f6040b1
Lizeth Gutierrez <lizethd.gutierrez11@gmail.com>
2023-02-02 07:39:22 -0600

    Configurar app en railway

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+// console.log(process.env)
+
 const express = require('express');
 const app = express();
 const initModels = require('./models/init.models');
@@ -9,7 +11,7 @@ initModels();
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Welcome!'}));
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server up and running. Listening on port ${PORT}.`);
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const db = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
     dialect: 'postgres'
 });
 
